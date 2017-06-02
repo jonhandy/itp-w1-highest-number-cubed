10fe388eb4f2d175026fc8da4915d33d2297f792
Jon Handy <jonhandy@gmail.com>
2017-06-02 00:44:11 +0000

    Jon Handy - completed cubey challenge

--- highest_number_cubed/main.py
+++ highest_number_cubed/main.py
@@ -2,4 +2,12 @@
 
 
 def highest_number_cubed(limit):
-    pass
+    candidate_number = 0
+    result_to_check = 0
+    
+    while result_to_check < limit:
+        candidate_number += 1
+        result_to_check = candidate_number ** 3
+        
+    
+    return candidate_number - 1
